5189f8528e35f6850c3c86105ef6c0cc42439051
aleroldan95 <alejandro.roldan@mercadolibre.com>
2021-08-24 15:18:51 -0300

    It works! :D

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index') #this is the index
+    path('', views.index, name='index'), #this is the index
+    path('add', views.addTodoItem, name='add'),
+    path('completed/<todo_id>', views.completedTodo, name='completed'),
+    path('deleteCompleted', views.deleteCompleted, name='deleteCompleted'),
+    path('deleteAll', views.deleteAll, name='deleteAll')
 ]
